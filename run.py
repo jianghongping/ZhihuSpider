46c3681327c68a011ccdc9210688fe91626a29a1
Milloy <yuerxmm@163.com>
2020-02-24 20:53:30 +0800

    development environment running entrance

---
+++ run.py
@@ -0,0 +1,10 @@
+import zhihu.spider
+from zhihu.conf import config
+
+config.warehouse(r'C:\Users\Milloy\Desktop')
+config.setting('running/file_type', 0)
+config.setting('running/cached', False)
+config.setting('running/css_output', False)
+config.setting('running/download_image', False)
+config.setting('running/cover', False)
+zhihu.spider.start(r'https://www.zhihu.com/question/371430700')

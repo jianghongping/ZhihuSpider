48756596a6f96030f29e9fc5c9264be4319db630
Milloy <yuerxmm@163.com>
2020-02-24 21:11:36 +0800

    development environment running entrance

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
 import zhihu.spider
 from zhihu.conf import config
 
-config.warehouse(r'C:\Users\Milloy\Desktop')
+# ### 程序设置（务必设置存储路径） #### #
+
+# 默认存储路径为用户文档，开发环境下可设置为用户桌面或其他路径，方便查看结果
+config.warehouse(r'C:\Users\{}\Desktop'.format('??'))
+
 config.setting('running/file_type', 0)
 config.setting('running/cached', False)
 config.setting('running/css_output', False)
 config.setting('running/download_image', False)
 config.setting('running/cover', False)
+
+# ### 启动爬虫 #### #
 zhihu.spider.start(r'https://www.zhihu.com/question/371430700')
